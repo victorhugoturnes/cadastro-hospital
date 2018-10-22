33ffe81fa32a1b487668ff40e984997f81ea56c3
korrescatherine <cathy.korres@gmail.com>
2018-10-22 20:33:18 -0300

    comentário updater

--- src/com/aed/trabalhos/algorithms/Updater.java
+++ src/com/aed/trabalhos/algorithms/Updater.java
@@ -5,7 +5,13 @@ import com.aed.trabalhos.estruturas.Register;
 
 import java.util.Scanner;
 
+// Classe para o metodo de Atualização
+// Classe para o metodo de Atualização
 public class Updater extends Algorithm {
+
+    // Sobreposição do metodo run
+    // Metodo generico pra execucao da funcao base da classe
+    // Eh chamado pela superclasse para generalizacao do main
     @Override
     public Btree run(Btree tree) {
         Scanner scan = new Scanner(System.in);
@@ -23,52 +29,52 @@ public class Updater extends Algorithm {
                 + "(I)endereco\n"
                 + "(K)data de nascimento"
         );
-        char control = scan.nextLine().charAt(0);
+        char control = scan.nextLine().charAt(0);   // Recolhe opção selecionada pelo usuario
         switch (control) {
             case 'A':
                 System.out.println("Por favor insira o nome");
-                doc.setNome(scan.nextLine());
+                doc.setNome(scan.nextLine());                   // Alteração de NOME
                 break;
             case 'B':
                 System.out.println("Por favor insira o sexo");
-                doc.setSexo(scan.nextLine());
+                doc.setSexo(scan.nextLine());                   // Alteração de SEXO
                 break;
             case 'C':
                 System.out.println("Por favor insira o cpf");
-                doc.setCpf(scan.nextLine());
+                doc.setCpf(scan.nextLine());                    // Alteração de CPF
                 break;
             case 'D':
                 System.out.println("Por favor insira o crm");
-                doc.setCrm(scan.nextLine());
+                doc.setCrm(scan.nextLine());                    // Alteração de CRM
                 break;
             case 'E':
                 System.out.println("Por favor insira a especialidade");
-                doc.setEspecialidade(scan.nextLine());
+                doc.setEspecialidade(scan.nextLine());          // Alteração de ESPECIALIDADE
                 break;
             case 'F':
                 System.out.println("Por favor insira o rg");
-                doc.setRg(scan.nextLine());
+                doc.setRg(scan.nextLine());                     // Alteração de RG
                 break;
             case 'G':
                 System.out.println("Por favor insira o telefone");
-                doc.setTelefone(scan.nextLine());
+                doc.setTelefone(scan.nextLine());               // Alteração de TELEFONE
                 break;
             case 'H':
                 System.out.println("Por favor insira o celular");
-                doc.setCelular(scan.nextLine());
+                doc.setCelular(scan.nextLine());                // Alteração de CELULAR
                 break;
             case 'i':
                 System.out.println("Por favor insira o email");
-                doc.setEmail(scan.nextLine());
+                doc.setEmail(scan.nextLine());                  // Alteração de E-MAIL
                 break;
             case 'J':
                 System.out.println("Por favor insira o endereco");
-                doc.setEndereco(scan.nextLine());
+                doc.setEndereco(scan.nextLine());               // Alteração de ENDEREÇO
                 break;
             default:
                 System.out.println("Por favor insira a data de nascimento");
-                doc.setDataNascimento(scan.nextLine());
+                doc.setDataNascimento(scan.nextLine());         // Alteração de DATA DE NASCIMENTO
         }
-        return tree;
+        return tree;    // Retorna Arvore B atualizada
     }
 }
