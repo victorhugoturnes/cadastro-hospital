da8135d5e0e8addc7bed56a7f2293e0a2053b79b
korrescatherine <cathy.korres@gmail.com>
2018-10-22 20:53:59 -0300

    comentário Search

--- src/com/aed/trabalhos/algorithms/Searcher.java
+++ src/com/aed/trabalhos/algorithms/Searcher.java
@@ -5,19 +5,26 @@ import com.aed.trabalhos.estruturas.Register;
 
 import java.util.Scanner;
 
+// Classe para os metodos de Busca
 public class Searcher extends Algorithm {
+
+    // Sobreposição do metodo run
+    // Método generico pra execução da função base da classe
+    // É chamado pela superclasse para generalização do main
     @Override
     public Btree run(Btree tree) {
-        System.out.println(find(tree));
+        System.out.println(find(tree)); // Busca e imprime uma Arvore B
         return tree;
     }
 
+    // Solicita um codigo do registro que sera buscado na Arvore B
     Register find(Btree tree) {
         System.out.println("please insert register to be found");
         return find(new Scanner(System.in).nextInt(), tree);
 
     }
 
+    // Retorna o registro
     Register find(int key, Btree tree){
         Btree find = tree.findPos(key);
         return find == null ? new Register() : find.getRegister(key);
